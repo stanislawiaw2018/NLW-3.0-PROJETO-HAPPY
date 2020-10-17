b99f248040afd1944896491db383acc2fbe7fe84
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2020-10-17 15:07:58 -0300

    Abstraçao da camada de controller, configuraçao do upload de imagens, validaçao dos dados e tratamento de erros e exceçoes

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+import './database/connection';
+import 'express-async-errors';
+import cors from 'cors';
+
+import routes from './routes';
+import errorHandler from './errors/handler';
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname,'..','uploads')))
+app.use(errorHandler);
+
+app.listen(3333);
